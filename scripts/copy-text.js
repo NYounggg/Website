0a2be264dddb99085aee3f98440b776c97ad96a0
NYounggg <nyoung3@unb.ca>
2024-10-28 16:36:12 -0300

    Added functionality to copy button

---
+++ scripts/copy-text.js
@@ -0,0 +1,8 @@
+﻿const copyButton = document.getElementById("copy-button");
+const tooltip = document.getElementById("copy-tooltip");
+
+copyText = function(text)
+{
+  navigator.clipboard.writeText(text);
+  console.log("copied " + text);
+}
